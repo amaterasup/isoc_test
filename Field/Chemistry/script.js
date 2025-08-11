e44a2117b8159955c09957297acb5001f4378d19
amaterasup <adibefendi309@gmail.com>
2025-08-11 10:30:18 +0700

    Update script.js

--- Field/Chemistry/script.js
+++ Field/Chemistry/script.js
@@ -48,7 +48,7 @@ function show(unsur){
 }
 function resizeContent() {
     const content = document.getElementsByClassName("periodic-table")[0];
-    const scale = window.innerWidth / 635;
+    const scale = window.innerWidth / 700;
     content.style.zoom = scale;
 }
 
@@ -276,4 +276,5 @@ function balanceEquation(compounds,unsur) {
     var rhs = new Array(matrix.length).fill(0);
     const solution = gaussianElimination(matrix);
     return solution;
+
 }
